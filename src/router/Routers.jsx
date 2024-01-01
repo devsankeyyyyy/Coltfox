60d100c497b3dfe75501ffafbd3cc0c939bbe951
Sanket Shinde <70633400+ssankey@users.noreply.github.com>
2024-01-01 10:17:39 +0530

    new commit

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -2,10 +2,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes,Link } from 'react-router-dom'; // Import BrowserRouter as Router
 
-import Generate from '../components/generator/Generator';
-import GenerateThen from '../components/generatedThen/GenerateThen';
-import Exp from '../components/exp/Exp';
-import Services from '../pages/Services.jsx';
 import Contact from '../pages/contact.jsx';
 import AboutUs from '../pages/about-us';
 import Who from '../pages/who.jsx';
@@ -25,12 +21,7 @@ import Kalacodeservices from '../pages/kalacode-services.jsx';
 const Routers = () => {
   return (
     <Routes>
-      {/* <Link to="/generate">Generate</Link> */}
-      <Route path='/' element={<Home />} />
-      <Route path='/generate-qr-code' element={<Generate />} />
-      <Route path='/generate-qr-code/:qrCodeId' element={<Generate />} />
-      <Route path='/generated-then' element={<GenerateThen/>}/>
-      <Route path='/services' element={<Services/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/about-us' element={<AboutUs/>}/>
       <Route path='/who' element={<Who/>}/>
